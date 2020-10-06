4fbec8a510a74fa8ef3739d0e8bb44aca5eb233c
ELEOUET Gaëtan <gaetan.eleouet@gmail.com>
2020-10-06 22:30:01 +0200

    first ihm

--- src/main/java/me/egaetan/mytwitt/MyClient.java
+++ src/main/java/me/egaetan/mytwitt/MyClient.java
@@ -13,7 +13,19 @@ public class MyClient {
 
 	
 	public static void main(String[] args) throws IOException, InterruptedException {
-		
+		System.out.println(	
+				" " + 
+				"with merged as ( " + 
+				" select top 100 author.name, message.message, message.time from message " + 
+				" inner join author on author.id = message.idauthor  " + 
+				" inner join follow on follow.idauthor = author.id  " + 
+				" where follow.idfollower = 41 " + 
+				" union " + 
+				" select top 100 author.name, message.message, message.time from message " + 
+				" inner join author on author.id = message.idauthor  " + 
+				" where author.id = 41 " + 
+				" order by time desc " + 
+				") select top 100  merged.name, merged.message, merged.time from merged order by time desc");
 		String host = "http://localhost:8081";
 		
 		CookieManager cookieManager = new CookieManager();
